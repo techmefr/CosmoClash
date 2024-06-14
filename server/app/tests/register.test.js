4b24be551061d14b160d07b9bf81d08946e638cc
Franck <136079691+Freddyede@users.noreply.github.com>
2024-06-14 22:26:25 +0200

    feat (S1_US_B_register) Adding new tests - Check req.body values are not null or undefined

--- server/app/tests/register.test.js
+++ server/app/tests/register.test.js
@@ -14,4 +14,20 @@ describe('register methods', () => {
     test('Check req.body keys are (username, email, password)', async () => {
         expect(Object.keys(request.body)).toBe(["username", "email", "password"]);
     })
+    test('Check req.body values are not null or undefined', async () => {
+        const values = [];
+        for (const [key, value] of Object.entries(request.body)) {
+            if(value !== undefined && value !== "") {
+                values.push(true);
+            } else {
+                values.push(false);
+            }
+        }
+        expect(values).equal([true, true, true]);
+        expect(values).notEqual([false, false, false]);
+        expect(values).notEqual([false, true, true]);
+        expect(values).notEqual([false, true, false]);
+        expect(values).notEqual([false, false, true]);
+        expect(values).notEqual([true, false, false]);
+    })
 });
