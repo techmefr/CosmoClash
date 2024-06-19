07273a24bdfdd07dc94f425015499402f7937d35
Franck <136079691+Freddyede@users.noreply.github.com>
2024-06-19 22:16:45 +0200

    feat(S1_US_B_actions): Adding constants actionModel -> instance of an action model

--- back/src/express/core/constants/index.js
+++ back/src/express/core/constants/index.js
@@ -1,3 +1,5 @@
 import UserModel from './../models/UserModel.js'
+import ActionModel from "../models/ActionModel.js";
 
 export const userModel = new UserModel();
+export const actionModel = new ActionModel();
