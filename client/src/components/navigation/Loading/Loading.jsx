92b8f6e51742d808a43e2b34683af1b3ee4599ed
techmefr <compigni.gaetan@gmail.com>
2024-06-20 15:32:15 +0200

    fix: add css in app.css and rename class in the component

--- client/src/components/navigation/Loading/Loading.jsx
+++ client/src/components/navigation/Loading/Loading.jsx
@@ -3,21 +3,21 @@ import "./loading.css";
 function Loading() {
   return (
     <>
-      <div className="container">
+      <div className="loading-container">
         <h1 className="title-loading">Loading</h1>
-        <div className="item" style={{ "--i": 0 }}></div>
-        <div className="item" style={{ "--i": 1 }}></div>
-        <div className="item" style={{ "--i": 2 }}></div>
-        <div className="item" style={{ "--i": 3 }}></div>
-        <div className="item" style={{ "--i": 4 }}></div>
-        <div className="item" style={{ "--i": 5 }}></div>
-        <div className="item" style={{ "--i": 6 }}></div>
-        <div className="item" style={{ "--i": 7 }}></div>
-        <div className="item" style={{ "--i": 8 }}></div>
-        <div className="item" style={{ "--i": 9 }}></div>
-        <div className="item" style={{ "--i": 10 }}></div>
-        <div className="item" style={{ "--i": 11 }}></div>
-        <div className="item" style={{ "--i": 12 }}></div>
+        <div className="loading-item" style={{ "--i": 0 }}></div>
+        <div className="loading-item" style={{ "--i": 1 }}></div>
+        <div className="loading-item" style={{ "--i": 2 }}></div>
+        <div className="loading-item" style={{ "--i": 3 }}></div>
+        <div className="loading-item" style={{ "--i": 4 }}></div>
+        <div className="loading-item" style={{ "--i": 5 }}></div>
+        <div className="loading-item" style={{ "--i": 6 }}></div>
+        <div className="loading-item" style={{ "--i": 7 }}></div>
+        <div className="loading-item" style={{ "--i": 8 }}></div>
+        <div className="loading-item" style={{ "--i": 9 }}></div>
+        <div className="loading-item" style={{ "--i": 10 }}></div>
+        <div className="loading-item" style={{ "--i": 11 }}></div>
+        <div className="loading-item" style={{ "--i": 12 }}></div>
       </div>
     </>
   );
