6de8765cf09afecb5f5498af95f807ad96c08200
Franck <136079691+Freddyede@users.noreply.github.com>
2024-06-20 10:29:57 +0200

    feat(S1_US2_B_planets): Adding updated model planet for adding position into this

--- back/src/express/core/models/PlanetModel.js
+++ back/src/express/core/models/PlanetModel.js
@@ -9,15 +9,9 @@ export default class PlanetModel extends AbstractModels {
             this.connexion.query(
                 `SELECT
                      p.name,
-                     r.name AS resource_name, r.icon as resource_icon,
-                     s.name AS ship_name,
                      pos.coord_x AS pos_coordX,
                      pos.coord_y AS pos_coordY
                 FROM planets p
-                    JOIN planets_resources pr on p.id = pr.planet_id
-                    JOIN resources r on r.id = pr.resource_id
-                    JOIN planets_ships ps on p.id = ps.planet_id
-                    JOIN ships s on ps.ship_id = s.id
                     JOIN positions pos on p.position_id = pos.id`,
                 (err, result) => {
                     if (err) reject(err);
@@ -31,15 +25,9 @@ export default class PlanetModel extends AbstractModels {
             this.connexion.query(`
                 SELECT 
                     p.name,
-                    r.name AS resource_name, r.icon as resource_icon,
-                    s.name AS ship_name,
                     pos.coord_x AS pos_coordX,
                     pos.coord_y AS pos_coordY
-                FROM planets p 
-                    JOIN planets_resources pr on p.id = pr.planet_id
-                    JOIN resources r on r.id = pr.resource_id
-                    JOIN planets_ships ps on p.id = ps.planet_id
-                    JOIN ships s on ps.ship_id = s.id
+                FROM planets p
                     JOIN positions pos on p.position_id = pos.id
                 WHERE p.id = ?`,
                 [id],
@@ -68,41 +56,27 @@ export default class PlanetModel extends AbstractModels {
     }
     putPlanets(planet, id) {
         return new Promise((resolve, reject) => {
-            this.connexion.query(`
-            UPDATE planets
-            SET name=?
-            WHERE id = ?
-        `,
-        [
-            action.name,
-            id
-        ],
-        (err, result) => {
-            if(err) {
-                reject(err);
-            }
-            resolve(result);
-        })
+            this.connexion.query(
+                `UPDATE planets SET name=? WHERE id = ?`,
+                [
+                    action.name,
+                    id
+                ],
+                (err, result) => {
+                    if(err) {
+                        reject(err);
+                    }
+                    resolve(result);
+                })
         });
     }
-    patchPlanet(planet, id) {
+    updatePositionId(planet, id) {
         return new Promise((resolve, reject) => {
-            this.connexion.query(`
-            UPDATE planets
-            SET name=?
-            WHERE id = ?
-        `,
-        [
-            action.name,
-            id
-        ],
-        (err, result) => {
-            if(err) {
-                reject(err);
-            }
-            resolve(result);
+            this.connexion.query('UPDATE planets SET position_id = ? WHERE planets.id = ?', [planet.positionId, id], (err, result) => {
+                if(err) reject(err)
+                resolve(result);
+            })
         })
-        });
     }
     deletePlanets(id) {
         return new Promise((resolve, reject) => {
