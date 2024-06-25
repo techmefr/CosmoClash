9054b600eefff7520e89f5ae024495b5000d32d8
messaoudisony <messaoudi.sonia89@gmail.com>
2024-06-25 14:53:53 +0200

    add model , service and controller for technologies

---
+++ back/src/express/app/admin/technologies/controller.js
@@ -0,0 +1,141 @@
+import {
+    technoFindAll,
+    technoFind,
+    technoCreated,
+    putTechno,
+    patchTechno,
+    deleteTechno
+} from "../../../core/services/TechnologiesService.js";
+
+export const index = (req, res) => {
+    technoFindAll()
+        .then(dataResult => {
+            if (dataResult.length > 0) {
+                res.status(200).send(JSON.stringify({
+                    message: 'Ok',
+                    data: dataResult,
+                    status: res.statusCode
+                }));
+            } else {
+                res.status(404).send(JSON.stringify({
+                    message: 'Not Found: Ships do not exist',
+                    status: res.statusCode
+                }));
+            }
+        })
+        .catch(err => {
+            res.status(400).send(JSON.stringify({
+                message: 'Bad request: ' + err.message,
+                status: res.statusCode
+            }));
+        });
+}
+
+export const findTechno = (req, res) => {
+    technoFind(req.params.id)
+        .then(dataResult => {
+            if (dataResult) {
+                res.status(200).send(JSON.stringify({
+                    message: 'Ok',
+                    data: dataResult,
+                    status: res.statusCode
+                }));
+            } else {
+                res.status(404).send(JSON.stringify({
+                    message: 'Not Found: Ship does not exist',
+                    status: res.statusCode
+                }));
+            }
+        })
+        .catch(err => {
+            res.status(400).send(JSON.stringify({
+                message: 'Bad request: ' + err.message,
+                status: res.statusCode
+            }));
+        });
+}
+
+export const createTechno = (req, res) => {
+    technoCreated(req.body)
+        .then(dataResult => {
+            if (dataResult) {
+                res.status(201).send(JSON.stringify({
+                    message: 'Created',
+                    status: res.statusCode
+                }));
+            } else {
+                res.status(500).send(JSON.stringify({
+                    message: 'Internal Server Error: bad credentials',
+                    status: res.statusCode
+                }));
+            }
+        })
+        .catch(err => {
+            res.status(400).send(JSON.stringify({
+                message: 'Bad request: ' + err.message,
+                status: res.statusCode
+            }));
+        });
+}
+
+export const puttechno = (req, res) => {
+    putTechno(req.body, req.params.id)
+        .then(resultData => {
+            if (resultData) {
+                res.status(200).send(JSON.stringify({
+                    message: 'Updated all ships',
+                    status: res.statusCode
+                }));
+            } else {
+                res.status(500).send(JSON.stringify({
+                    message: 'Internal Server Error: bad credentials',
+                    status: res.statusCode
+                }));
+            }
+        })
+        .catch(err => {
+            res.status(400).send(JSON.stringify({
+                message: 'Bad request: ' + err.message,
+                status: res.statusCode
+            }));
+        });
+}
+
+export const patchOneTechno = (req, res) => {
+    patchTechno(req.body, req.params.id)
+        .then(resultData => {
+            if (resultData) {
+                res.status(200).send(JSON.stringify({
+                    message: 'Updated specific ship data',
+                    status: res.statusCode
+                }));
+            } else {
+                res.status(500).send(JSON.stringify({
+                    message: 'Internal Server Error: bad credentials',
+                    status: res.statusCode
+                }));
+            }
+        })
+        .catch(err => {
+            res.status(400).send(JSON.stringify({
+                message: 'Bad request: ' + err.message,
+                status: res.statusCode
+            }));
+        });
+}
+
+export const shipDelete = (req, res) => {
+    deleteTechno(req.params.id)
+        .then(() => {
+            res.status(200).send(JSON.stringify({
+                message: 'Deleted successfully',
+                status: res.statusCode
+            }));
+        })
+        .catch(err => {
+            res.status(400).send(JSON.stringify({
+                message: 'Bad request: ' + err.message,
+                status: res.statusCode
+            }));
+        });
+}
---
+++ back/src/express/core/services/TechnologiesService.js
@@ -0,0 +1,8 @@
+import { technologiesModel } from "../constants/index.js";
+
+export const technoFindAll = () => technologieModel.readAllTechnologies().then();
+export const technoFind = id => technologieModel.readOneTechnologie(id).then();
+export const technoCreated = () => technologieModel.createNewTechnologies (technologie).then();
+export const putTechno = (technologie, id) => technologieModel.putTechnologies(...technologie, id).then();
+export const patchTechno = (technologie, id) => technologieModel.patchTechnologies(...technologie, id).then();
+export const deleteTechno = id => technologieModel.deleteTechnologie(id).then();
