002fa514ad4e85ff83b977344713d135ee33ced6
Franck <136079691+Freddyede@users.noreply.github.com>
2024-07-08 16:13:39 +0200

    feat(S3_US1_update_seed): Added: - Message, - Members Updated: - constructor(), - CRUD

--- back/src/express/core/models/AllianceModel.js
+++ back/src/express/core/models/AllianceModel.js
@@ -5,7 +5,7 @@ export default class AllianceModel extends AbstractModels {
     //     super(props);
     // }
     constructor() {
-        super();
+    super();
     }
     readAllAlliances() {
         return new Promise((resolve, reject) => {
@@ -26,17 +26,15 @@ export default class AllianceModel extends AbstractModels {
     createNewAlliance (alliance) {
         return new Promise((resolve, reject) => {
             return this.connexion.query(
-                `
-                    INSERT INTO alliances(name, color)
-                    VALUES(?, ?)
-                `,
+                `INSERT INTO alliances(role_id, name, color) VALUES(?, ?, ?)`,
                 [
+                    alliance.role_id,
                     alliance.name,
                     alliance.color
                 ],
                 (err, result) => {
-                    if(err) reject(err)
-                    resolve(result);
+                if(err) reject(err)
+                resolve(result);
                 }
             );
         });
@@ -52,39 +50,92 @@ export default class AllianceModel extends AbstractModels {
         }
         sql += 'WHERE id = ?';
         return new Promise((resolve, reject) => {
-            this.connexion.query(sql, [...values, id], (err, result) => {
-                if(err) {
-                    reject(err);
+            this.connexion.query(
+                sql,
+                [
+                    ...values,
+                    id
+                ],
+                (err, result) => {
+                    if(err) {
+                        reject(err);
+                    }
+                    resolve(result);
                 }
-                resolve(result);
-            })
+            )
         })
     }
     putAlliances(resource, id) {
         return new Promise((resolve, reject) => {
             this.connexion.query(`
-            UPDATE alliances
-            SET name=?, 
+                UPDATE alliances
+                SET name=?, 
                 color=?
-            WHERE id = ?
-        `, [
-                resource.name,
-                resource.icon,
-                id
-            ], (err, result) => {
-                if(err) {
-                    reject(err);
+                WHERE id = ?
+                `,
+                [
+                    resource.name,
+                    resource.icon,
+                    id
+                ],
+                (err, result) => {
+                    if(err) reject(err);
+                    resolve(result);
                 }
-                resolve(result);
-            })
+            )
         });
     }
     deleteAlliances(id) {
         return new Promise((resolve, reject) => {
-            this.connexion.query('DELETE FROM alliances WHERE id=?', [id], (err, result) => {
-                if(err) reject(err)
-                resolve(result);
-            })
+            this.connexion.query(
+                'DELETE FROM alliances WHERE id=?',
+                [id],
+                (err, result) => {
+                    if(err) reject(err)
+                    resolve(result);
+                }
+            )
+        });
+    }
+
+    /**
+    * addMember
+    * @version 0.0.1
+    * @author Patouillard Franck<patouillardfranck3@gmail.com>
+    */
+    addMember(allianceMembers) {
+        return new Promise((resolve, reject) => {
+            this.connexion.query(
+                `INSERT INTO alliance_members (user_id, alliance_id) VALUES (?, ?)`,
+                [
+                    allianceMembers.user_id,
+                    allianceMembers.alliance_id
+                ],
+                (err, results) => {
+                    if (err) reject(err);
+                    resolve(results);
+                }
+            )
+        })
+    }
+    /**
+    * Messages
+    * @version 0.0.1
+    * @author Patouillard Franck<patouillardfranck3@gmail.com>
+    */
+    addMessages(allianceMessage) {
+        return new Promise((resolve, reject) => {
+            this.connexion.query(
+                `INSERT INTO alliance_messages (alliance_id, message_id) VALUES (?, ?)`,
+                [
+                    allianceMessage.alliance_id,
+                    allianceMessage.message_id
+                ],
+                (err, results) => {
+                    if (err) reject(err);
+                    resolve(results);
+                }
+            )
         })
     }
 }
