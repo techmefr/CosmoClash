871d0c081c16aa15873115a5f4cf63ff968e6f53
techmefr <compigni.gaetan@gmail.com>
2024-07-10 11:03:11 +0200

    feat: Add Two pages, ChoosePage to choose a planet and GamePage to screen info about user

---
+++ client/src/pages/GamePage/GamePage.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./gamePage.css";
+
+export default function GamePage() {
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8002/api/users");
+        setUserData(response.data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!userData) {
+    return <div>No user data found.</div>;
+  }
+
+  return (
+    <div className="cards">
+      <div className="card black">
+        <p className="tip">Nom d&apos;utilisateur</p>
+        <p className="second-text">{userData.username}</p>
+      </div>
+      <div className="card black">
+        <p className="tip">Alliance</p>
+        <p className="second-text">{userData.alliance_name}</p>
+      </div>
+      <div className="card black">
+        <p className="tip">Nombre de planètes</p>
+        <p className="second-text">{userData.planets_count}</p>
+      </div>
+      <div className="card black">
+        <p className="tip">Nombre de vaisseaux</p>
+        <p className="second-text">{userData.ships_count}</p>
+      </div>
+      <div className="card black">
+        <p className="tip">Énergie</p>
+        <p className="second-text">{userData.energy}</p>
+      </div>
+      <div className="card black">
+        <p className="tip">Monnaie</p>
+        <p className="second-text">{userData.money}</p>
+      </div>
+      <div className="card black">
+        <p className="tip">Matériel</p>
+        <p className="second-text">{userData.material}</p>
+      </div>
+    </div>
+  );
+}
