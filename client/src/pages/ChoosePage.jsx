5d25914e8384ab37051031d2c049d4d69d6c00fd
techmefr <compigni.gaetan@gmail.com>
2024-07-11 15:21:19 +0200

    init ChoosePage

--- client/src/pages/ChoosePage.jsx
+++ client/src/pages/ChoosePage.jsx
@@ -1,38 +1,59 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CustomCard from "../components/ui/CustomCard/CustomCard";
-import { PlanetContext } from "../hook/PlanetContext/";
+import { PlanetContext } from "../hook/PlanetContext";
 import ElectricityIco from "../components/ui/svg/ElectricityIco";
 import MoneyBagIco from "../components/ui/svg/MoneyBagIco";
 import MineIco from "../components/ui/svg/MineIco";
+import PlanetIcoRandom from "../components/ui/PlanetIcoRandom";
+import fetchPlanetData from "../services/planetService";
+import "./gamePage.css";
+import CommunauteIco from "../../components/ui/svg/CollaborationIco";
+import RocketIco from "../../components/ui/svg/RocketIco";
 
-function ChoosePage() {
-  const { planetsData, loading, error } = useContext(PlanetContext);
+export default function ChoosePage() {
+  const [planetData, setPlanetData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getPlanetData = async () => {
+      try {
+        const data = await fetchPlanetData();
+        setPlanetData(data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getPlanetData();
+  }, []);
 
   if (loading) {
-    return <p>Chargement des données...</p>;
+    return <div>Loading...</div>;
   }
 
   if (error) {
-    return <p>Erreur de chargement des données : {error.message}</p>;
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!planetData) {
+    return <div>No user data found.</div>;
   }
 
   return (
-    <div className="choose-page">
-      {planetsData.map((planet) => (
-        <CustomCard key={planet.id} title={planet.nom}>
-          <div>
-            <ElectricityIco /> Énergie: {planet.resources.energy}
-          </div>
-          <div>
-            <MoneyBagIco /> Monnaie: {planet.resources.money}
-          </div>
-          <div>
-            <MineIco /> Matériel: {planet.resources.material}
-          </div>
-        </CustomCard>
+    <div className="cards">
+      <h2>Bienvenue {userData.username}</h2>
+      {dataFields.map((field, index) => (
+        <CustomCard
+          key={index}
+          title={field.title}
+          value={planetData[field.key]}
+          icon={field.icon}
+          iconProps={field.iconProps}
+        />
       ))}
     </div>
   );
 }
-
-export default ChoosePage;
