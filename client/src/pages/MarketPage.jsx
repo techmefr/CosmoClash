a7add03766c2f7d1fc4e322a4d1b4a61677cf3dd
Messaoud ZOUGGAB <messaoud.zouggab@gmail.com>
2024-07-11 10:30:42 +0200

    title and background added

--- client/src/pages/MarketPage.jsx
+++ client/src/pages/MarketPage.jsx
@@ -1,9 +1,18 @@
-// TODO :
+import Starfield from "react-starfield";
 
 export default function MarketPage() {
   return (
-    <div>
-      <h1>page market</h1>
-    </div>
-  )
+    <>
+      <div className="flex items-center justify-center flex-col ">
+        <Starfield
+          starCount={20000}
+          starColor={[255, 255, 255]}
+          speedFactor={0.1}
+          backgroundColor="black"
+        />
+
+        <h3 className="text-white text-4xl font-bold m-8">ACHATS</h3>
+      </div>
+    </>
+  );
 }
